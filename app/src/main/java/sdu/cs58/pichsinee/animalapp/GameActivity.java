804aa9a9d34b0e770759e547da76283d05f50dc2
wattana <armbombom01@gmail.com>
2018-03-14 10:42:22 +0700

    Initial commit

---
+++ app/src/main/java/sdu/cs58/pichsinee/animalapp/GameActivity.java
@@ -0,0 +1,117 @@
+package sdu.cs58.pichsinee.animalapp;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class GameActivity extends AppCompatActivity {
+
+    //Explicit
+    Button btn1,btn2,btn3,btn4;
+    ImageView questionImageView;
+    ImageButton volumnImageButton;
+    MediaPlayer mediaPlayer;
+    int questionCount = 1;
+    ArrayList<Integer> qID = new ArrayList<Integer>();
+    String answer;
+    int score = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        //Initial view
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        questionImageView = findViewById(R.id.imvQuestion);
+        volumnImageButton = findViewById(R.id.imbVolumn);
+
+        //แสดงคำถาม
+        for (int i=1; i <= questionCount; i++) {
+            qID.add(i);
+        }
+        Collections.shuffle(qID);   //กำหนดการ random คำถาม
+        setQuestion(qID.remove(0));
+    }//end onCreate Method
+
+    private void setQuestion(int qID) { // method setQuestion ทำหน้าที่กำหนดข้อคำถ่มและเฉลยในแต่ละข้อ
+
+        if (qID == 1) {
+            answer = "นก";
+            questionImageView.setImageResource(R.drawable.bird);
+            mediaPlayer = MediaPlayer.create(this, R.raw.bird);
+
+            ArrayList<String> choice = new ArrayList<String>();
+            choice.add("นก");
+            choice.add("ม้า");
+            choice.add("แมว");
+            choice.add("หมู");
+            Collections.shuffle(choice);
+            btn1.setText(choice.remove(0));
+            btn2.setText(choice.remove(0));
+            btn3.setText(choice.remove(0));
+            btn4.setText(choice.remove(0));
+        }
+
+
+    }//end setQuestion Method
+
+    public void choiceAns (View view){ //สำหรับไว้ตรวจคำตอบ
+
+        Button button = (Button) view;
+        String buttonString = button.getText().toString();
+        if (buttonString.equals(answer)){
+            score++;
+        }
+
+        if (qID.isEmpty()) {//ถ้าทำครบทุกข้อเเล้ว qid เป็นค่าว่าง
+            dialogboxScore();//สำหรับเเสดงคะเเนนรวม
+        }else {//ถ้ายังทำไม่ครบทุกข้อ
+            setQuestion(qID.remove(0));//เรียก method  setQuestion เเสดงค่าคำถามถัดไป
+        }
+
+
+    }//end choiceAns
+
+    private void dialogboxScore() {// Method สำหรับเเสดงคะเนน
+        AlertDialog.Builder builder =  new AlertDialog.Builder(this);
+        builder.setTitle("สรุปคะเเนน");
+        builder.setMessage("ได้คะเเนน "+score+" คะแนน")
+            .setCancelable(false)
+            .setPositiveButton("ออกจากเกม",new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();//ปิด Activity ออกจากแอป
+                }
+            })
+            .setNegativeButton("เล่นอีกครั้ง", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = getIntent();
+                    finish();
+                    startActivity(intent);//กำหนดให้สร้าง
+                }
+            });
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();//กำหนดให้เเสดง
+
+    }// end dialogboxScore method
+
+    public void playSound(View view) {
+        mediaPlayer.start();
+    }//end playSound Method
+
+}// end Class
